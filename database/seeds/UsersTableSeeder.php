b9e1a4285168954b7a26132f55101c32c6fef191
basakest <ab1652759879@gmail.com>
2019-08-05 06:58:16 +0000

    用户激活

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,7 +17,7 @@ class UsersTableSeeder extends Seeder
 
         $user = User::find(1);
         $user->name = "test";
-        $user->email = 'test@example.com';
+        $user->email = '1652759879@qq.com';
         $user->is_admin = true;
         $user->save();
     }
