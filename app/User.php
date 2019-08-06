508351a146fa158f35f8d1f43a56a4cfe91e910c
basakest <ab1652759879@gmail.com>
2019-08-06 14:24:24 +0000

    社交统计信息

--- app/User.php
+++ app/User.php
@@ -66,28 +66,28 @@ class User extends Authenticatable
 
     public function followers()
     {
-        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
     }
 
     public function followings()
     {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
     }
 
     public function follow($user_ids)
     {
-        if(! is_array($user_ids)) {
-            $user_ids = compact($user_ids);
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
         }
         $this->followings()->sync($user_ids, false);
     }
 
     public function unfollow($user_ids)
     {
-        if( ! is_array($user_ids)){
-            $user_ids = compact($user_ids);
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
         }
-        $this->followings->detach($user_ids);
+        $this->followings()->detach($user_ids);
     }
 
     public function isFollowing($user_id)
